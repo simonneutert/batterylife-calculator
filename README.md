051e1f5e96b3e6bd9abf4f8cac276c59acc93a14
Simon Neutert <simonneutert@users.noreply.github.com>
2022-12-30 19:22:51 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # batterylife-calculator
+
+Battery logic extraced and polished from https://of-things.de.
+
+## Examples
+
+Basic usage: make sure to cast to Floats or Integers when using.  
+⚠️ commas in input fields need to be sanitized ⚠️
+
+```javascript
+var x = new BatteryLifeCalculator(
+  10,   // time the code runs in seconds
+  120,  // time the code sleeps
+  100,  // average power consumption in mAh when code runs
+  10,   // average power consumption in mAh when code sleeps
+  4400, // total capacity of the battery in mAh
+  20    // percentage of the the total capacity you want as buffer (20 % is a sane default)
+)
+const powerPerHourMilliAmp = x.powerEstimatedHourly()
+console.log(powerPerHour) // #=> 16.923076923076923
+
+const life = x.calculate()
+console.log(life)
+// {
+//   powerAverage:16.923076923076923,
+//   runtimeDaysEstimated:8,
+//   runtimeDaysRemainingHoursEstimated:16,
+//   runtimeHoursEstimated:208
+// }
+```
+
+Example from MilliAmpHours to MicroAmpHours
+
+```javascript
+const milliAmps = 1000
+new BatteryLifeCalculator().milliAmpToMicroAmp(milliAmps)
+
+const microAmps = 1000
+new BatteryLifeCalculator().microAmpToMilliAmp(microAmps)
+```
--- main.js
+++ main.js
@@ -1,144 +1,106 @@
-// set variables used
-var timerunvalue
-var timesleepvalue
-var consumpactivevalue
-var consumpsleepvalue
-var powerlipobruvalue
-var powerliposafevalue
-var unitconsumpsleep
-var sleepconsump
-var solutionHeight
-
-var getValues = function () {
-  timerunvalue = parseFloat($('#timerun').val().replace(/\,/g, '.'))
-  timesleepvalue = parseFloat($('#timesleep').val().replace(/\,/g, '.'))
-  consumpactivevalue = parseFloat($('#consumpactive').val().replace(/\,/g, '.'))
-  consumpsleepvalue = parseFloat($('#consumpsleep').val().replace(/\,/g, '.'))
-  powerlipobruvalue = parseFloat($('#powerlipobru').val().replace(/\,/g, '.'))
-  powerliposafevalue = parseFloat($('#powerliposafe').val().replace(/\,/g, '.'))
-  unitconsumpsleep = parseFloat($('#unitconsumpsleep').attr('data-unit'))
-
-  return 'Values complete'
-}
-
-var roundoff = function (x) {
-  return Math.round(x * 100.0) / 100.0
-}
-
-var calcpowerlipo = function (x, y) {
-  return parseFloat((x * (100 - y)) / 100)
-}
-var calcruns = function (x, y) {
-  return parseFloat(60 / (x + y))
-}
-var calcrunshour = function (x, y) {
-  return parseFloat(3600 / (x + y))
-}
-var calcpowerrun = function (x, y) {
-  return parseFloat((x / (x + y)) * 3600)
-}
-var calcpowersleep = function (x, y) {
-  return parseFloat((y / (x + y)) * 3600)
-}
-var calcpowerest = function (a, b, c, d) {
-  return parseFloat((a / 3600) * b + (c / 3600) * d)
-}
-var sleepconsump = function () {
-  return parseFloat(consumpsleepvalue * unitconsumpsleep)
-}
-
-var powerlipo = function () {
-  return calcpowerlipo(powerlipobruvalue, powerliposafevalue)
-}
-var runs = function () {
-  return calcruns(timerunvalue, timesleepvalue)
-}
-var runshour = function () {
-  return calcrunshour(timerunvalue, timesleepvalue)
-}
-var powerrun = function () {
-  return calcpowerrun(timerunvalue, timesleepvalue)
-}
-var powersleep = function () {
-  return calcpowersleep(timerunvalue, timesleepvalue)
-}
-var powerest = function () {
-  return calcpowerest(
-    powerrun(),
-    consumpactivevalue,
-    powersleep(),
-    sleepconsump(),
-  )
-}
-var runtimehoursest = function () {
-  return parseInt(powerlipo() / powerest())
-}
-var runtimedaysest = function () {
-  return parseInt(runtimehoursest() / 24)
-}
-var runtimedayshoursest = function () {
-  return parseInt(runtimehoursest() % 24)
-}
-
-// calculate the results with every user interaction
-$('form').keyup(function () {
-  dothemath()
-})
-$('form').keydown(function () {
-  dothemath()
-})
-
-// enable consumption unit switching
-$('#unitconsumpsleep').click(function () {
-  switch (this.getAttribute('data-unit')) {
-    case '1.0':
-      this.setAttribute('data-unit', '0.001')
-      this.innerHTML = '&micro;A'
-      console.log(this.getAttribute('data-unit'))
-      break
-    case '0.001':
-      this.setAttribute('data-unit', '1.0')
-      this.innerHTML = 'mA'
-      console.log(this.getAttribute('data-unit'))
-      break
+class BatteryLifeCalculator {
+  constructor(
+    timeRunSeconds,
+    timeSleepSeconds,
+    consumptionActiveMilliAmpHours,
+    consumptionSleepMilliAmpHours,
+    powerBatteryTotalMilliAmpHours,
+    powerBatteryBufferBeforeEmptyPercent = 20) {
+    this.timeRunSeconds = timeRunSeconds
+    this.timeSleepSeconds = timeSleepSeconds
+    this.consumptionActiveMilliAmpHours = consumptionActiveMilliAmpHours
+    this.consumptionSleepMilliAmpHours = consumptionSleepMilliAmpHours
+    this.powerBatteryTotalMilliAmpHours = powerBatteryTotalMilliAmpHours
+    this.powerBatteryBufferBeforeEmptyPercent = powerBatteryBufferBeforeEmptyPercent
   }
-  dothemath()
-})
 
-// output the results in html
-var dothemath = function () {
-  if (
-    $('#timerun').val() &&
-    $('#timesleep').val() &&
-    $('#consumpactive').val() &&
-    $('#consumpsleep').val() &&
-    $('#powerlipobru').val() &&
-    $('#powerliposafe').val()
-  ) {
-    getValues()
-    console.log(getValues())
-    $('#solutions').fadeIn()
-    if (runtimehoursest() > 48) {
-      $('.runtimedaysest').html(
-        runtimedaysest().toString() +
-          ' days and ' +
-          runtimedayshoursest().toString() +
-          ' hours',
-      )
-    }
-    if (runtimehoursest() < 24) {
-      $('.runtimedaysest').html('less than a day')
-    }
-    if (runtimehoursest() > 24 && runtimehoursest() < 48) {
-      $('.runtimedaysest').html('between one and two days')
-    }
+  // public API
 
-    $('.runtimehoursest').html(runtimehoursest().toString() + ' hours')
-    $('.powerest').html(roundoff(powerest()).toString() + ' mAh')
-    $.scrollTop($('#solutions').height())
-  } else {
-    $('#solutions').fadeOut()
+  milliAmpToMicroAmp(milliAmps) {
+    return milliAmps * 1000
+  }
+
+  microAmpToMilliAmp(milliAmps) {
+    return milliAmps * 0.001
+  }
+
+  calculate() {
+    return {
+      powerAveragePerHour: this.powerEstimatedHourly(),
+      runtimeHoursEstimated: this.runtimeHoursEstimated(),
+      runtimeDaysEstimated: this.runtimeDaysEstimated(),
+      runtimeDaysRemainingHoursEstimated: this.runtimeDaysRemainingHoursEstimated()
+    }
+  }
+
+  powerEstimatedHourly() {
+    return this.calcPowerEst(
+      this.powerRun(),
+      this.consumptionActiveMilliAmpHours,
+      this.powerSleep(),
+      this.consumptionSleepMilliAmpHours
+    )
+  }
+
+  runtimeHoursEstimated() {
+    return parseInt(this.powerLipo() / this.powerEstimatedHourly())
+  }
+
+  runtimeDaysEstimated() {
+    return parseInt(this.runtimeHoursEstimated() / 24)
+  }
+
+  runtimeDaysRemainingHoursEstimated() {
+    return parseInt(this.runtimeHoursEstimated() % 24)
+  }
+
+  // private
+
+  roundOff(x) {
+    return Math.round(x * 100.0) / 100.0
+  }
+
+  calcPowerLipo(x, y) {
+    return parseFloat((x * (100 - y)) / 100)
+  }
+
+  calcRuns(x, y) {
+    return parseFloat(60 / (x + y))
+  }
+
+  calcRunsHour(x, y) {
+    return parseFloat(3600 / (x + y))
+  }
+
+  calcPowerRun(x, y) {
+    return parseFloat((x / (x + y)) * 3600)
+  }
+
+  calcPowerSleep(x, y) {
+    return parseFloat((y / (x + y)) * 3600)
+  }
+
+  powerLipo() {
+    return this.calcPowerLipo(this.powerBatteryTotalMilliAmpHours, this.powerBatteryBufferBeforeEmptyPercent)
+  }
+
+  runs() {
+    return this.calcRuns(this.timeRunSeconds, this.timeSleepSeconds)
+  }
+
+  runsHour() {
+    return this.calcRunsHour(this.timeRunSeconds, this.timeSleepSeconds)
+  }
+
+  powerRun() {
+    return this.calcPowerRun(this.timeRunSeconds, this.timeSleepSeconds)
+  }
+
+  powerSleep() {
+    return this.calcPowerSleep(this.timeRunSeconds, this.timeSleepSeconds)
+  }
+
+  calcPowerEst(a, b, c, d) {
+    return parseFloat((a / 3600) * b + (c / 3600) * d)
   }
 }
-
-dothemath()
